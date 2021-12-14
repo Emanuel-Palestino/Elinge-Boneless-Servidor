39c83fce750ea5aa064383f0fc769197b14c3199
Avelitro <joseabelmartinezleyva@gmail.com>
2021-12-13 20:07:58 -0600

    Cambios

---
+++ src/routes/clientesRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { clientesController } from '../controllers/clientesController';
+class ClientesRoutes
+{
+    public router: Router=Router();
+    constructor()
+    {
+        this.config();
+    }
+    config() : void
+    {
+        this.router.get('/', clientesController.listar);
+        this.router.get('/:id', clientesController.listarUno );
+        this.router.post('/crear', clientesController.crear );
+        this.router.put('/actualizar/:idCliente', clientesController.actualizar);
+        this.router.delete('/eliminar/:idCliente', clientesController.eliminar );
+    }
+}
+const clientesRoutes= new ClientesRoutes();
+export default clientesRoutes.router;
